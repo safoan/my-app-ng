1cdbc4f9f5e3ab09d2c0567abcd769dfc7674953
safoan bazine <safibazine@gmail.com>
2023-12-04 17:14:00 +0000

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ import { HousingLocation } from '../housing-location';
   </form>
 </section>
 <section class ="results">
-  <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation] = "housingLocation">
+  <app-housing-location *ngFor="let housingLocation of housingLocationList" [HousingLocation] = "housingLocation">
   </app-housing-location>
 
 </section>
