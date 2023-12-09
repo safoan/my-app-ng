72213b535dc43c28170080b2777b3d468ffcf238
safoan bazine <safibazine@gmail.com>
2023-12-09 07:13:59 +0000

    added more code to make the home icon routes to the home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Routes ,Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing.modules';
 
+
+const routes :Routes =[
+  { path : 'home' , component:HomeComponent},
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , HomeComponent , RouterModule],
+  imports: [CommonModule, RouterOutlet , HomeComponent , AppRoutingModule ,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-APP';
+  
+  constructor(private router: Router) {}
+
+  goToHome() {
+    this.router.navigate(['']);
+  }
 }
+
